952785bbc123e30fa25954e4360009c959d36267
ashnachadha <ashnachadha@gmail.com>
2020-01-16 14:03:18 -0800

    Add CMake and main

--- c-echo.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(int argv, char**  argc) {
-	//Skip the first agc index because its the program
-	for(int i = 1; i < argv; i++) {
-		std::cout << argc[i] << " ";
-	}
-	std::cout << std::endl;
-}
